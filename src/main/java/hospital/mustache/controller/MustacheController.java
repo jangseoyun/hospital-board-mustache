c480b57a17944a6dd5c66ffc6e177faf723dc145
seoyun <may-primavera@naver.com>
2022-11-07 11:46:34 +0900

    feat: controller 생성, mustache html

---
+++ src/main/java/hospital/mustache/controller/MustacheController.java
@@ -0,0 +1,24 @@
+package hospital.mustache.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping("/index")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "seoyun");
+        return "greetings";
+    }
+
+    @GetMapping("/hi/{id}")
+    public String mustacheCon(Model model, @PathVariable("id") int id) {
+        model.addAttribute("username", "seoyun");
+        model.addAttribute("id", id);
+        return "greetings";
+    }
+
+}
