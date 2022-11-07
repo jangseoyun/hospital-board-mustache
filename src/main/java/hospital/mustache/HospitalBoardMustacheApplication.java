70a6098544143636cb22481f67eca063947c3194
seoyun <may-primavera@naver.com>
2022-11-07 11:21:24 +0900

    프로젝트 세팅

---
+++ src/main/java/hospital/mustache/HospitalBoardMustacheApplication.java
@@ -0,0 +1,13 @@
+package hospital.mustache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalBoardMustacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalBoardMustacheApplication.class, args);
+    }
+
+}
