d92652ebb2e23c7268451fcbce6f51f5d1da20ad
seoyun <may-primavera@naver.com>
2022-11-07 15:22:13 +0900

    feat: Article Entity 생성

---
+++ src/main/java/hospital/mustache/domain/entity/Article.java
@@ -0,0 +1,27 @@
+package hospital.mustache.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String contents;
+
+    public Article(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+}
